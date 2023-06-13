ec40a759a09aee8b426182a25904479db3e95cf3
Sonia Baptista <sonia.baptista095@gmail.com>
2023-06-14 00:01:39 +0200

    modify security controller and create registrationType and my template security/registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
@@ -30,10 +34,20 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/registration', name: 'registration')]
-    public function registration()
+    public function registration(Request $request, UserRepository $userRepository)  
     {
-
-
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $form->getData();
+            $userRepository->save($user, true);
+            return $this->redirectToRoute('app_security_login');
+        }
+        return $this->render('security/registration.html.twig',[
+            'form'=> $form->createView()
+        ]);
     }
-
 }
