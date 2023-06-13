46a5f60edf07b1d787a8ad83b0731b58151c5178
Sonia Baptista <sonia.baptista095@gmail.com>
2023-06-13 15:13:07 +0200

    Feature creation login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+#[Route('/security', name: 'app_security_')]
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    public function logout()
+    {
+        // nothing to put inside because symfony do that 
+    }
+
+    public function registration()
+    {
+        
+
+    }
+
+}
